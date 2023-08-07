3cd5ee51f2cdbeb6a299f9fc0f0e7f8325305343
Giersz <krystian.gierzzewski3@gmail.com>
2023-08-07 18:03:03 +0200

    tak

---
+++ regulamin/index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/regulamin.css">
+    <link rel="shortcut icon" href="../img/logo.svg" type="image/svg">
+    <title>KonikBus</title>
+</head>
+<body>
+    <a href="https://konikbus.pl">
+        <header>
+            <img src="../img/logo_name.svg" alt="KonikBus">
+        </header>
+    </a>
+    <main>
+        <div id="regulamin">
+            <h1>Regulamin przewozu osób i rzeczy<br>oraz usług oferowanych<br>przez Firmę KonikBus tel. +48 510 31 9999</h1><br><br>
+            <h2>§ 1. ZAKRES ZOBOWIĄZANIA</h2>
+            <p>1. Regulamin przewozu oraz świadczenia naszych usług, rezerwacji itp. określa warunki:<br>
+            a) dokonania rezerwacji za pośrednictwem telefonu, e-mail, sms, komunikatorów
+            społecznościowych oraz formularza dostępnego na naszej stronie,<br>
+            b) przewozu osób i rzeczy, wypożyczania aut oraz pozostałych usług
+            ustanowionych w CEiDG, są nadzorowane przez naszą Firmę.<br>
+            2. Wszelkie informacje dotyczące warunków korzystania z naszych usług są opisane w niniejszym
+            regulaminie.</p>
+            <h2>§ 2. OBOWIĄZKI PRZEWOŹNIKA</h2>
+            <p>1. Przewoźnik zobowiązuje się do dołożenia wszelkich starań, aby przewieźć Pasażera i jego bagaż w
+            czasie, jaki został wyznaczony. W przypadkach awaryjnych niewynikających z winy firmy, lecz z
+            przypadków losowych, Przewoźnik zobowiązuje się zapewnić transport zastępczy np. u innego
+            przewoźnika, zgodnie z zamówieniem.<br>
+            2. Przewoźnik zobowiązuje się do przewozu Pasażera i jego bagażu w wyznaczone miejsce,
+            maksymalna ilość bagaży w cenie to dwie walizki do 20 kg plus bagaż podręczny do 5 kg.<br>
+            3. Dokładne godziny wyjazdu są podawane dzień przed wyjazdem. Z uwagi na specyfikę
+            działalności przewoźnika (przewozy Door to Door) godziny wyjazdu i przyjazdu mogą ulec
+            zmianie, nie są objęte gwarancją i nie stanowią części umowy przewozu.<br>
+            4. Przewoźnik nie ponosi odpowiedzialności za opóźnienia i ich dalsze skutki spowodowane
+            czynnikami niezależnymi od niego np. kontrole policyjne, wypadki, korki, problemy techniczne,
+            warunki atmosferyczne, utrudnienia w ruchu drogowym lub na przejściach granicznych.<br>
+            5. W sytuacjach niezależnych od firmy, awarie itp. Przewoźnik może zmienić miejsca wysadzenia
+            adresatów oraz zapewnić dojazd na miejsce posiłkując się innymi firmami przewozowymi, z
+            którymi współpracuje.<br>
+            6. Przewoźnik jest uprawniony do odmowy wykonania przewozu, lub kontynuacji, bez konieczności
+            zwrotu kosztów poniesionych przez Pasażera w związku z podróżą, w przypadku gdy Pasażer
+            nie przestrzega warunków umowy przewozu w tym niniejszego Regulaminu:<br>
+            a) znajduje się w stanie wskazującym na spożycie alkoholu lub środków odurzających,<br>
+            b) zachowuje się w sposób uciążliwy dla współpasażerów, zagrażając bezpieczeństwu i zdrowiu
+            współpasażerów,<br>
+            c) nie posiada wymaganych dokumentów potrzebnych do przekroczenia granicy lub nie stosuje
+            się do przepisów celno-dewizowych, bądź też nie został wpuszczony do kraju przez władze
+            imigracyjne, przewozi towary, które ze względu na ilość lub rodzaj wymagają dłuższej odprawy
+            granicznej i tym samym mogą powodować opóźnienia w odprawie pojazdu na granicy.<br>
+            7. Przewoźnik zastrzega, iż nie ponosi on odpowiedzialności z tytułu podania błędnego adresu
+            przez Pasażera.<br>
+            8. Przewoźnik nie odpowiada za odwołania przewozów z przyczyn od niego niezależnych np.
+            zamknięcie granic, ze względu na potrzeby obronności lub bezpieczeństwa państwa bądź w
+            wypadku klęski żywiołowej czy ataków terrorystycznych.<br>
+            9. Za nie odbycie podróży z winy Przewoźnika Podróżnemu przysługuje zwrot kwoty stanowiącej
+            równowartość opłaconej podróży.<br>
+            10. Przewoźnik ma prawo do wprowadzenia przesiadek podczas przejazdu ze względów
+            logistycznych lub bezpieczeństwa podróżnych.<br>
+            11. Przewoźnik oświadcza, iż Pasażer objęty jest ubezpieczeniem od następstw nieszczęśliwych
+            wypadków (NNW) Przewoźnika. Ubezpieczenie to obejmuje wyłącznie sytuacje, jakie mogą
+            zdarzyć się w trakcie pobytu Pasażera w pojeździe. Po zaistnieniu wypadku szkody należy
+            zgłaszać do biura firmy.</p>
+            <h2>§ 3. OBOWIĄZKI PASAŻERA</h2>
+            <p>1. Pasażer jest zobowiązany posiadać ważny dokument uprawniający go do przekroczenia granicy
+            oraz wszelkich innych wymaganych dokumentów i zaświadczeń niezbędnych do odbycia podróży.<br>
+            2. Pasażer powinien przybyć (oczekiwać) pod wskazany Przewoźnikowi adres 15 minut przed
+            ustaloną przez Przewoźnika godziną wyjazdu, jak również posiadać aktywny numer telefonu podany
+            do kontaktu.<br>
+            3. Pasażer jest ponadto zobowiązany do złożenia bagażu we wskazane przez kierowcę Przewoźnika
+            miejsce, stosowania się do poleceń Kierowcy.<br>
+            4. Pasażer jest zobowiązany okazywania, na wezwanie uprawnionych służb granicznych i celnych
+            wymaganych dokumentów tj.
+            podanie dokładnego adresu, kodu pocztowego, ulicy, nr budynku, miejscowości, kraju, w pisowni
+            państwa, którego przewóz dotyczy;<br>
+            5. Pasażer jest zobowiązany do zapinania pasów bezpieczeństwa w pojeździe.<br>
+            6. Pasażer jest zobowiązany do opisania swojego bagażu poprzez czytelne umieszczenie takich
+            danych jak imię, nazwisko i adres zamieszkania.<br>
+            7. O ile przepisy kraju docelowego i krajów tranzytowych podróży, nie stanowią inaczej, dzieci do 12
+            roku życia muszą podróżować pod opieką osoby dorosłej. W przypadku, gdy dorosły nie jest
+            rodzicem bądź prawnym opiekunem, konieczne jest posiadanie przez niepełnoletniego
+            „Upoważnienia do przewozu osoby niepełnoletniej” wystawionego i wydanego wraz z biletem przez
+            sprzedawcę. Osoby w wieku od 12 do 16 lat mogą podróżować samodzielnie, lecz muszą posiadać
+            potwierdzony notarialnie dokument „Upoważnienie do przewozu osoby niepełnoletniej” podpisany
+            przez oboje rodziców lub opiekunów prawnych. W szczególnych przypadkach przewoźnik może
+            zastosować odstępstwa od zasad opisanych w niniejszym punkcie.<br>
+            8. Fakt nieoczekiwania Pasażera pod wskazanym adresem w dniu wyjazdu w określonym przedziale
+            czasowym jest równoznaczny z rezygnacją z przejazdu. Przewoźnik ma prawo do żądania od
+            Pasażera 100 % zwrotu wartości przejazdu.<br>
+            9. W pojeździe obowiązuje całkowity zakaz palenia tytoniu i spożywania napojów alkoholowych.<br>
+            10. W trakcie przerw w podróży Pasażer ma obowiązek przestrzegać wyznaczonego uprzednio
+            czasu zakończenia przerwy i nie powodować przez swoje zaniedbanie konieczności wszczynania
+            poszukiwań bądź opóźnień w odjeździe.<br>
+            11. Pasażer jest zobowiązany do przestrzegania przepisów porządkowych obowiązujących w
+            transporcie oraz zaleceń kierowcy pojazdu Przewoźnika.<br>
+            12. Pasażer odpowiada za wszelkie szkody wyrządzone Przewoźnikowi, szczególnie za zniszczenia
+            wyposażenia pojazdu i innym Pasażerom na zasadach ogólnych przewidzianych przepisami
+            Kodeksu Cywilnego.<br>
+            13. Pasażer, który w sposób umyślny lub wskutek zaniedbania zanieczyszcza pojazd Przewoźnika,
+            jest zobowiązany do zapłaty na rzecz Przewoźnika kwoty stanowiącej równowartość kosztów, które
+            musi ponieść Przewoźnik w celu naprawienia lub wyczyszczenia pojazdu.<br>
+            14. Koszt przejazdu jest ustalany podczas rezerwacji według aktualnego cennika przewozu.</p>
+            <h2>§ 4. BAGAŻ</h2>
+            <p>1. Pasażer ma prawo do bezpłatnego przewozu maksymalnie 2 sztuk bagażu podstawowego oraz
+            jeden bagaż podręczny zgodnie z poniższymi wytycznymi:<br>
+            a) dwóch sztuk bagażu podstawowego o wadze do 20 kg każdy, przewożonego w luku bagażowym,<br>
+            b) jedna sztuka bagażu podręcznego o wadze do 5 kg.<br>
+            2. Bagaż podręczny to bagaż w wymiarach pozwalających na swobodne umieszczenie go pod
+            siedzeniem, a jego rozmiary nie powinny utrudniać swobody podróży pozostałym pasażerom.<br>
+            3. Przewoźnik ma prawo, odmówić zabrania bagażu, jeżeli nie spełnia on wymogów określonych w
+            niniejszym regulaminie. Ma prawo odmówić, także zabrania dodatkowego bagażu w przypadku, gdy
+            zabranie go jest niemożliwe i wpływa negatywnie na jakość podróży innych pasażerów.<br>
+            4. Nadbagaż powinien być zgłoszony przy rezerwacji biletu, doliczana jest dodatkowa opłata.<br>
+            5. Przewoźnik informuje, iż zabrania się przewozu bagażu, jak również posiadania przy sobie
+            materiałów i przedmiotów, które mogą zagrażać bezpieczeństwu w transporcie, środków
+            niedozwolonych, nieakcyzowanych, niezgodnych z prawem oraz rozporządzeniami.<br>
+            6. Przewoźnik nie ponosi odpowiedzialności za bagaże znajdujące się poza lukiem bagażowym oraz
+            za bagaż podręczny.<br>
+            7. Przewoźnik nie odpowiada za bagaże Pasażerów.<br>
+            8. Każda sztuka bagażu przewożona w luku bagażowym powinna być wyraźnie oznakowana przez
+            Pasażera trwałą przewieszką z podanym nazwiskiem i adresem. Kierowca może sprawdzić
+            zgodność podanych danych z dokumentem tożsamości Pasażera. Bagaż nieopisany i którego nie
+            można przypisać do konkretnej osoby, może zostać przez załogę usunięty z pojazdu. W takiej
+            sytuacji wykluczone jest dochodzenie roszczeń za utratę bagażu.</p>
+            <h2>§ 5. PRZEWÓZ PACZEK</h2>
+            <p> 1.Przesyłka, nadana za pośrednictwem naszej Firmy oraz zatwierdzona do przewozu przez
+            Przewoźnika, jest odbierana spod wskazanego adresu w danej miejscowości i dostarczana do
+            wskazanego miejsca.<br>
+            2. Zleceniodawca zgłaszając odbiór przesyłki potwierdza znajomość niniejszego Regulaminu.<br>
+            3. Czas dostawy przesyłki wynosi od 2 do 6 dni roboczych.<br>
+            4. W przypadku gdy Odbiorca odmówi przyjęcia przesyłki, jest ona zwracana Nadawcy. Koszty
+            przewozu (w obie strony) ponosi Nadawca.<br>
+            5. W przypadku wykonywania usługi przewozu przesyłek na zlecenie Zleceniodawcy i na jego koszt,
+            w razie odmowy uiszczenia należności na rzecz Firmy przez Zleceniodawcę, do jej zapłaty
+            zobowiązany jest Nadawca.<br>
+            6. Nadawca jest zobowiązany przekazać Przewoźnikowi przesyłkę w stanie umożliwiającym jej
+            dostarczenie. Nadawca jest zobowiązany odpowiednio przygotować przesyłkę tzn.:<br>
+            a) powinna być zamknięta i zabezpieczona skutecznym środkiem takim jak taśma samoprzylepna, plomby
+            czy pieczęcie lakowe,<br>
+            b) powinna być odpowiednio wytrzymała,<br>
+            c) powinna być opisana gdy wymagają tego właściwości lub zawartość przesyłki, poprzez oznakowanie np.
+            "góra/dół" lub "uwaga szkło". W opakowaniu przesyłki nie wolno stosować zabezpieczeń
+            mogących uszkodzić tę lub inne przewożone przesyłki<br>
+            7. Przewoźnik zastrzega przewożenia paczek nie z godnym z prawem przewozu, typu środki niedozwolone (np. narkotyki itp.), zagrażające życiu, bez akcyzy.<br>
+            8. Opakowanie przesyłki wlicza się do jej masy. Cena jest ustalana w trakcie przyjęcia zlecenia.<br>
+            9. Każda przesyłka powinna być zaopatrzona w dane Odbiorcy i Nadawcy:<br>
+            a) imię i nazwisko<br>
+            b) ulicę i numer domu<br>
+            c) kod pocztowy<br>
+            d) miejscowość<br>
+            e) telefon kontaktowy<br>
+            10. Za otwarcie przesyłki podczas kontroli przez służby bezpieczeństwa, celne itp. Przewoźnik nie
+            odpowiada.<br>
+            11. Za wszelkie przewożone rzeczy w paczce odpowiada Nadawca, a także Odbiorca przesyłki.<br>
+            12. Przewoźnik nie odpowiada, za rzeczy przewożone w paczce.</p>
+            <h2>§ 6. PRZEWÓZ OSÓB ZE ZWIERZĘTAMI</h2>
+            <p>1. Przewóz osoby ze zwierzęciem należy zgłosić w chwili rezerwacji, zostanie podjęta decyzja w
+            zależności od zwierzaka. Koszt przewozu zwierzaka jest ustalany w chwili rezerwacji.<br>
+            2. We wcześniej zgłoszonych przypadkach Przewoźnik może przewieźć zwierzę, tylko pod
+            warunkiem jego małych gabarytów, braku zagrożenia i bezpośredniej opieki nad nim Pasażera.
+            Przewóz taki należy bezwzględnie zgłosić wcześniej oraz zapewnić transporter dla zwierzaka we
+            własnym zakresie.</p>
+            <h2>§ 7. UMOWA PRZEWOZU</h2>
+            <p>1. Pasażer zobowiązany jest do złożenia rezerwacji u Przewoźnika: telefonicznie, za pomocą strony
+            internetowej Przewoźnika, sms, bądź za pośrednictwem komunikatorów
+            społecznościowych.<br>
+            2. Umowa przewozu zostaje zawarta w momencie przyjęcia rezerwacji telefonicznej lub internetowej
+            po potwierdzeniu przez osobę przyjmującą rezerwację informacją zwrotną. Ustne i SMS-owe
+            zawarcie umowy przez złożenie rezerwacji jest tak samo wiążące jak zawarcie umowy na piśmie.<br>
+            3. Opłatę należy uiścić przed rozpoczęciem podróży gotówką u Kierowcy lub przelewem na konto
+            podane przez Przewoźnika.<br>
+            4. W momencie złożenia rezerwacji Pasażer akceptuje treść niniejszego regulaminu.<br>
+            5. Z uwagi na specyfikę działalności przewoźnika, czyli przewóz osób i mienia od drzwi do drzwi, nie
+            istnieje regularny i stały rozkład jazdy. Rozkład jazdy ustalany jest na bieżąco na podstawie
+            Rezerwacji dokonywanych na bieżąco. Godziny przekazywane w trakcie rezerwacji są przybliżone.<br>
+            6. Dokładna godzina przejazdu zostaje podana Pasażerowi dzień przed wyjazdem.<br>
+            7. Rezerwacja potwierdzona przez Pasażera dzień przed wyjazdem do godz. 12:00 obliguje
+            opłaceniem niniejszej rezerwacji, pomimo że zrezygnował po potwierdzeniu rezerwacji.<br>
+            8. Jeżeli Pasażer nagle zrezygnuje z przejazdu, również jest zobligowany do opłacenia przejazdu.<br>
+            9. Przewoźnik może powierzyć wykonanie przewozu innym przewoźnikom, informując o tym fakcie
+            Pasażera.<br>
+            10. Rezerwacja przewozu jest równoznaczna z zapoznaniem i zaakceptowaniem niniejszego
+            regulaminu.</p>
+            <h2>§ 8. REKLAMACJA</h2>
+            <p>1. Wszelkie zgłoszenia reklamacyjne związane z realizacją umowy przewozowej należy kierować na
+            adres e-mail konikbus@gmail.com lub listownie na adres Przewoźnika.<br>
+            2. Przewoźnik przyjmuje reklamacje zgłoszone w okresie do 14 dni od zaistnienia okoliczności, która
+            jest przedmiotem reklamacji. Przy zgłaszaniu reklamacji niezbędne jest opisanie zaistniałych
+            sytuacji, wnoszone zastrzeżenia oraz doznane szkody i związane z tym roszczenia.<br>
+            3. Przewoźnik rozpatrzy reklamacje w terminie 14 dni roboczych od dnia jej otrzymania, z założeniem,
+            że wszelkie zażalenia zostaną dokładnie opisane (można też dołączyć dokumentację zdjęciową,
+            bądź nagranie video)<br>
+            4. Przewoźnik zastrzega, iż reklamacje dotyczące bagażu przewożonego w luku bagażowym można
+            składać tylko w chwili jego wydania bezpośrednio osobie wydającej bagaż.<br>
+            5. Bagaż nieodebrany przez Pasażera, lub omyłkowo odebrany inny bagaż Pasażera, Przewoźnik za
+            to nie odpowiada.<br>
+            6. Przedmioty pozostawione przez Pasażera w pojeździe w wyniku nieuwagi lub w innych
+            okolicznościach nie są objęte ochroną i odpowiedzialnością Przewoźnika.</p>
+            <h2>§ 9. UMOWA NA POZOSTAŁE USŁUGI</h2>
+            <p>1. Pozostałe usługi oferowane przez Firmę będą regulowane według indywidualnie sporządzanych
+            umów na potrzeby Klienta tj.:<br>
+            a) wypożyczenie pojazdów z kierowcą lub bez,<br>
+            b) usługa auto-lawety,<br>
+            c) przeprowadzki.</p>
+            <h2>§ 10. PODSTAWA PRAWNA</h2>
+            <p>1. Niniejsze postanowienia stanowią regulamin w rozumieniu art. 4 ustawy z dnia 15 listopada 1984 r.
+            prawo przewozowe (Dz. U. z 2000 r. nr 50, poz. 601; z późniejszymi zmianami) i określa warunki obsługi
+            podróżnych, oraz przewozu osób i bagażu.<br>
+            2. Aktualnie obowiązujący Regulamin jest publikowany na stronie internetowej Przewoźnika oraz w jego
+            siedzibie i flocie transportowej.<br>
+            3. W zakresie nieuregulowanym w niniejszym regulaminie mają zastosowanie odpowiednie przepisy
+            ustawy z dnia 15 listopada 1984 r. prawo przewozowe (Dz. U. z 2000 r. nr 50, poz. 601; z późn.
+            zmianami oraz ustawy z dnia 23 kwietnia 1964 r. kodeks cywilny (Dz. U. z 1964 r. nr 16, poz. 93; z późn.
+            zmianami)).<br>
+            4. W sprawach nieregulowanych niniejszą umową obowiązują przepisy Kodeksu Cywilnego.</p>
+        </div>
+    </main>
+    <footer>
+        <iframe id="mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d591.5489552339001!2d17.23118784120886!3d53.62564690454602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4702317d8095dbf7%3A0x13f7424c0f25c105!2sChrz%C4%85stowo%2029A%2C%2077-300%20Chrz%C4%85stowo!5e0!3m2!1spl!2spl!4v1691049892222!5m2!1spl!2spl" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <span class="socialmedia">
+            <pre>
+<h3>Znajdziesz nas również na:</h3>
+• <a href="https://www.facebook.com/profile.php?id=100095320358728" target="blank">Facebook</a>
+• <a href="https://www.instagram.com/konikbus/" target="blank">Instagram</a>
+• <a href="https://www.tiktok.com/@konikbuskonik" target="blank">TikTok</a>
+• <a href="https://youtube.com">YouTube</a>
+<h3>Skontaktuj się z nami!</h3>
+• Tel.: <a href="tel:+48 510319999">+48 510 319 999</a>
+• E-mail: <a href="mailto:konikbus@gmail.com">konikbus@gmail.com</a>
+• Adres: <a href="https://goo.gl/maps/7xuJdQHSPyjCcQDn8">Chrząstowo 29A, 77-300 Człuchów</a>
+            </pre>
+        </span>
+        <span class="others">
+            <pre>
+            • <a href="../regulamin/">Regulamin</a>
+            </pre>
+        </span>
+        <iframe id="mapa2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d591.5489552339001!2d17.23118784120886!3d53.62564690454602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4702317d8095dbf7%3A0x13f7424c0f25c105!2sChrz%C4%85stowo%2029A%2C%2077-300%20Chrz%C4%85stowo!5e0!3m2!1spl!2spl!4v1691049892222!5m2!1spl!2spl" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="dummy"></div>        
+    </footer>
+</body>
+</html>
